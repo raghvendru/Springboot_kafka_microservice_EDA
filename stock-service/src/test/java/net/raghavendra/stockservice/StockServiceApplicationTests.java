4c3edf46a8b94a635b9fbd8383315847ecf705ff
raghavendru <raghavendruppar@gmail.com>
2023-09-04 15:59:26 +0530

    sb using kafka microservice

---
+++ stock-service/src/test/java/net/raghavendra/stockservice/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.raghavendra.stockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
